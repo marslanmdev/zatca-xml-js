7bcb63fa7f7ea835c958c19c91c348e21af11349
marslanmdev <marslanmdev@gmail.com>
2026-03-09 12:45:15 +0500

    export issue fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from "./zatca/egs";
-// export * from "./zatca/ZATCASimplifiedTaxInvoice";
+export * from "./zatca/ZATCASimplifiedTaxInvoice";
 // export * from "./zatca/ZATCAStandardTaxInvoice";
-export { ZATCASimplifiedTaxInvoice } from "./zatca/ZATCASimplifiedTaxInvoice";
+// export { ZATCASimplifiedTaxInvoice } from "./zatca/ZATCASimplifiedTaxInvoice";
 export { ZATCAStandardTaxInvoice } from "./zatca/ZATCAStandardTaxInvoice";
 export { generatePhaseOneQR } from "./zatca/qr";
